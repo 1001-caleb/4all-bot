2fe0be799510280ffc09c14324c31062a92da49d
Caleb Parrra <mccalebyt@gmail.com>
2022-08-04 09:16:09 -0500

    slash commands handler

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,31 +1,22 @@
-const { SlashCommandBuilder, Routes } = require("discord.js");
+const { Routes } = require("discord.js");
 const { REST } = require("@discordjs/rest");
+const fs = require('node:fs');
+const path = require('node:path');
 
 const { BOT } = require("./config.js");
 
-const commands = [
-    new SlashCommandBuilder().setName("ping").setDescription("replies with pong"),
-    new SlashCommandBuilder()
-        .setName("server")
-        .setDescription("replies with server info"),
-    new SlashCommandBuilder()
-        .setName("user")
-        .setDescription("replies with user info! "),
-].map((command) => command.toJSON());
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands/slash');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: "10" }).setToken(BOT.token);
 
-(async () => {
-	try {
-		console.log('Started refreshing application (/) commands.');
-
-		await rest.put(
-			Routes.applicationGuildCommands(BOT.clientId, BOT.guildId),
-			{ body: commands },
-		);
-
-		console.log('Successfully reloaded application (/) commands.');
-	} catch (error) {
-		console.error(error);
-	}
-})();
+rest.put(Routes.applicationGuildCommands(BOT.clientId, BOT.guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-const { Client, GatewayIntentBits } = require("discord.js");
-
-const { BOT } = require("./config.js");
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const fs = require('node:fs');
+const path = require('node:path');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { BOT } = require("./config.js");
+
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands/slash');
+const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandsFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    client.commands.set(command.data.name, command);
+}
 
 client.once("ready", () => {
     console.log(`${client.user.username} is ready!`);
@@ -11,24 +23,15 @@ client.once("ready", () => {
 client.on("interactionCreate", async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
 
-    const { commandName } = interaction;
+    const command = client.commands.get(interaction.commandName);
+    
+    if (!command) return;
 
-    if (commandName === "ping") {
-        await interaction.reply("Pong!");
-    } else if (commandName === "server") {
-        await interaction.reply(
-            `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
-        );
-    } else if (commandName === "user") {
-        await interaction.reply(
-            `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
-        );
-    }
-});
-
-client.on("messageCreate", (message) => {
-    if (message.content.startsWith(".ping")) {
-        message.channel.send("Pong");
+    try{
+        await command.execute(interaction);
+    }catch (error){
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
 });
 
