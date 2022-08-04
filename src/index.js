f784817c2734a3095ce8bde07cc51a0af9bbb43d
Caleb Parrra <mccalebyt@gmail.com>
2022-08-04 14:08:54 -0500

    event handler

--- src/index.js
+++ src/index.js
@@ -9,16 +9,24 @@ const { BOT } = require("./config.js");
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands/slash');
 const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
 for (const file of commandsFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
     client.commands.set(command.data.name, command);
 }
 
-client.once("ready", () => {
-    console.log(`${client.user.username} is ready!`);
-});
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 client.on("interactionCreate", async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
